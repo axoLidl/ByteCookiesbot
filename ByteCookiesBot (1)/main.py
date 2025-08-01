b2101ba9c0e8046bf53281357886d83068c8b4e1
axoLidl <ivana.saveleva@gmail.com>
2025-08-01 16:59:00 +0700

    Update main.py

--- ByteCookiesBot (1)/main.py
+++ ByteCookiesBot (1)/main.py
@@ -1,11 +1,24 @@
-from aiogram import Bot, Dispatcher, executor, types
+import os
+import asyncio
+from aiogram import Bot, Dispatcher
 from handlers.main import register_handlers
-from utils.config import BOT_TOKEN
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise ValueError("Error: BOT_TOKEN is not set in environment variables.")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 register_handlers(dp)
 
+async def main():
+    try:
+        print("Starting bot...")
+        await dp.start_polling()
+    finally:
+        await bot.close()
+
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
